4fa4c87d844eb5bd9d5f87632b5a926266e1f99a
Yuji Oshiro <iamyujioshiro@gmail.com>
2023-12-11 11:51:22 -0800

    Update feature commands and add word count feature

--- journal.py
+++ journal.py
@@ -3,6 +3,7 @@ import os
 import sys
 from datetime import datetime
 import subprocess
+import time
 
 # Default configuration settings
 DEFAULT_CONFIG = {
@@ -33,7 +34,8 @@ def first_time_setup():
     print("Welcome to the Journal App! It looks like this is your first time.")
     print("When you open this app, you can begin writing immediately.")
     print("When you are finished writing, press CTRL+D to save the journal entry.")
-    print("There are several commands you can use, such as EDIT, DELETE, or SETTINGS.")
+    print("There are several commands you can use, such as VIEW, EDIT, DELETE, or SETTINGS.")
+    print("Type 'VIEW' and ENTER to view existing journal entries.")
     print("Type EDIT and press ENTER to edit a previously written journal entry.")
     print("Type DELETE and press ENTER to delete a previously written journal entry.")
     print("Type SETTINGS and press ENTER to configure some settings. This will open up a vim instance in terminal, so make sure you know how to use vim!")
@@ -58,45 +60,51 @@ def write_entry(entry_content):
     if entry_content.strip().lower() == 'help':
         display_help()
         return
-    
     entry_id = datetime.now().strftime(CONFIG['filename_format'])
     save_entry(entry_id, entry_content)
-    print(f"\nEntry saved with ID: {entry_id}")
+    word_count = count_words(entry_content)
+    print(f"Entry saved with ID: {entry_id}. Word count: {word_count}")
+
+def list_entries():
+    """Lists all journal entries and returns a list of file names."""
+    entries = [f for f in os.listdir("./journal_entries") if f.endswith('.json')]
+    for i, entry in enumerate(entries, start=1):
+        print(f"{i}. {entry}")
+    return entries
+
+def view_entry():
+    entries = list_entries()
+    choice = int(input("Enter the number of the entry you want to view: "))
+    with open(os.path.join("./journal_entries", entries[choice - 1]), 'r') as file:
+        entry = json.load(file)
+        entry_content = entry['content']
+        print("\nJournal entry:")
+        print(entry_content)
 
 def edit_entry():
-    entry_id = input("Enter the entry ID to edit: ")
-    file_path = get_entry_filename(entry_id)
-    if os.path.exists(file_path):
-        with open(file_path, 'r+') as file:
-            entry = json.load(file)
-            print("\nCurrent entry content:")
-            print(entry['content'])
-            print("\nEdit your entry (press CTRL+D when finished):\n")
-            new_content, _ = get_multiline_input("")
-            if new_content:
-                entry['content'] = new_content
-                file.seek(0)
-                json.dump(entry, file, indent=4)
-                file.truncate()
-                print("Entry updated.")
-            else:
-                print("No changes made.")
-    else:
-        print("Entry not found.")
+    entries = list_entries()
+    choice = int(input("Enter the number of the entry you want to edit: "))
+    with open(os.path.join("./journal_entries", entries[choice - 1]), 'r+') as file:
+        entry = json.load(file)
+        print("\nCurrent content:")
+        print(entry['content'])
+        
+        new_content = input("\nEnter new content for the entry:\n")
+        entry['content'] = new_content  # Update the content in the entry
 
+        file.seek(0)
+        json.dump(entry, file, indent=4)  # Write the updated JSON back to the file
+        file.truncate()
 
 def delete_entry():
-    entry_id = input("Enter the entry ID to delete: ")
-    file_path = get_entry_filename(entry_id)
-    try:
-        os.remove(file_path)
-        print("Entry deleted.")
-    except FileNotFoundError:
-        print("Entry not found.")
+    entries = list_entries()
+    choice = int(input("Enter the number of the entry you want to delete: "))
+    os.remove(os.path.join("./journal_entries", entries[choice - 1]))
 
 def display_help():
     print("Welcome to the help menu. Here are the available commands:")
     print("Type your journal entry. When finished writing, press CTRL+D to save it.")
+    print("Type 'VIEW' and ENTER to view existing journal entries.")
     print("Type 'EDIT' and ENTER to edit a previously written journal entry.")
     print("Type 'DELETE' and ENTER delete an existing entry.")
     print("Type 'SETTINGS' and ENTER to modify the configuration file in vim.")
@@ -119,7 +127,10 @@ def get_multiline_input(prompt_message):
             line = input()
             if not lines:  # Check the command if it's the first line
                 command = line.strip().lower()
-                if command == 'edit':
+                if command == 'view':
+                    view_entry()
+                    return None, True
+                elif command == 'edit':
                     edit_entry()
                     return None, True
                 elif command == 'delete':
@@ -135,6 +146,29 @@ def get_multiline_input(prompt_message):
         except EOFError:
             break  # CTRL+D or CTRL+Z was pressed
     return '\n'.join(lines), False
+
+def count_words(entry_content, file_path='../Minesweeper/journalentry.txt'):
+    # Write the journal entry content to the specified file
+    with open(file_path, 'w') as temp_file:
+        temp_file.write(entry_content)
+
+    # Wait for the file to be updated by the microservice
+    previous_modification_time = os.path.getmtime(file_path)
+    while True:
+        time.sleep(1)  # Polling interval
+        current_modification_time = os.path.getmtime(file_path)
+        if current_modification_time != previous_modification_time:
+            break  # File has been updated
+    
+    # Read the updated word count
+    with open(file_path, 'r') as temp_file:
+        word_count_str = temp_file.read().strip()
+
+    try:
+        return int(word_count_str)
+    except ValueError:
+        print("Error in reading word count.")
+        return 0
 
 def main():
     clear_screen()
